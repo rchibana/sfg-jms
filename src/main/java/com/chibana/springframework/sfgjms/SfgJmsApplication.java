89cc19090a014675bcb5f9c285aaa4096d9eae81
Rodrigo Chibana <rodrigo.chibana@gmail.com>
2020-04-04 17:35:24 -0300

    Changed the application to stop using embedded activeMQ and start using an external instead

--- src/main/java/com/chibana/springframework/sfgjms/SfgJmsApplication.java
+++ src/main/java/com/chibana/springframework/sfgjms/SfgJmsApplication.java
@@ -1,21 +1,12 @@
 package com.chibana.springframework.sfgjms;
 
-import org.apache.activemq.artemis.core.config.impl.ConfigurationImpl;
-import org.apache.activemq.artemis.core.server.ActiveMQServers;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SfgJmsApplication {
 
-    public static void main(String[] args) throws Exception {
-
-        ActiveMQServers.newActiveMQServer(new ConfigurationImpl()
-            .setPersistenceEnabled(false)
-            .setJournalDirectory("target/data/journal")
-            .setSecurityEnabled(false)
-            .addAcceptorConfiguration("invm", "vm://0"));
-
+    public static void main(String[] args) {
         SpringApplication.run(SfgJmsApplication.class, args);
     }
 
